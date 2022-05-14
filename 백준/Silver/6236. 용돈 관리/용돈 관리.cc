56f6fc3916aca8eb7d940cd98c0ff97675e67dd2
jini-coding <96573160+jini-coding@users.noreply.github.com>
2022-05-15 06:58:07 +0900

    Update 용돈 관리.cc

--- 백준/Silver/6236. 용돈 관리/용돈 관리.cc
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -1,47 +1,62 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
-int n, m;
-int *req;
+using namespace std;
 
-int search(int withdrawal_unit) {
+int N, M, num;
+vector<int> money;
+
+bool check(int n) {
     int cnt = 1;
-    int sum = 0;
+    int temp = n;
     
-    for (int i = 0; i < n; ++i) {
-        if (sum + req[i] > withdrawal_unit) {
+    for (int i = 0; i < N; i++) {
+        if (money[i] <= n)
+            n -= money[i];
+        else {
+            n = temp;
             cnt++;
-            sum = req[i];
-        } else {
-            sum += req[i];
+            
+            if (money[i] > n)
+                return false;
+            
+            n -= money[i];
+        }
+    }
+    return cnt <= M;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> N >> M;
+    
+    for (int i = 0; i < N; i++) {
+        cin >> num;
+        money.push_back(num);
+    }
+    
+    int left = 1;
+    int right = 10000;
+    int result = 0, mid = 0;
+    
+    while (left <= right) {
+        mid = (left + right) / 2;
+        
+        if (check(mid)) {
+            right = mid - 1;
+            result = mid;
+        }
+        
+        else {
+            left = mid + 1;
         }
     }
     
-    return cnt;
-}
-
-int main(int argc, const char *argv[]) {
-    std::cin >> n >> m;
-    req = new int[n] {0, };
-    
-    int req_max = 0;
-    for (int i = 0; i < n; ++i) {
-        std::cin >> req[i];
-        req_max = std::max(req[i], req_max);
-    }
-    
-    int left = req_max;
-    int right = 1e9;
-    int mid;
-    
-    while (left < right) {
-        if (search(mid = (left + right) / 2) > m)
-            left = mid + 1;
-        else
-            right = mid;
-    }
-    
-    std::cout << left << std::endl;
+    cout << result << "\n";
     
     return 0;
 }
