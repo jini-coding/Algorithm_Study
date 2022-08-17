003cd9328fdfc88a1acd4ac380950f206462a5a9
jini-coding <96573160+jini-coding@users.noreply.github.com>
2022-08-17 21:42:28 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+struct node
+{
+    char left;
+    char right;
+};
+
+struct node arr[100];
+
+void preOrder(char root)
+{
+    if (root == '.')
+        return;
+    else {
+        cout << root;
+        preOrder(arr[root].left);
+        preOrder(arr[root].right);
+    }
+}
+
+void inOrder(char root)
+{
+    if (root == '.')
+        return;
+    else {
+        inOrder(arr[root].left);
+        cout << root;
+        inOrder(arr[root].right);
+    }
+}
+
+void postOrder(char root)
+{
+    if (root == '.')
+        return;
+    else {
+        postOrder(arr[root].left);
+        postOrder(arr[root].right);
+        cout << root;
+    }
+}
+
+
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    char t1, t2, t3;
+    
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> t1 >> t2 >> t3;
+        arr[t1].left = t2;
+        arr[t1].right = t3;
+    }
+
+    preOrder('A');
+    cout << "\n";
+    inOrder('A');
+    cout << "\n";
+    postOrder('A');
+    cout << "\n";
+    
+    return 0;
+}
